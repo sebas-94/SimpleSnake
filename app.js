e555cb3cca876ccd3ab3fc77ce7cbe906ffee2d1
sebas-94 <sebas.solis.94@gmail.com>
2020-09-03 22:14:51 +0200

    add score

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 window.onload = function () {
-    canv = document.getElementById("gc");
+    canv = document.getElementsByTagName("canvas")[0];
     ctx = canv.getContext("2d");
     document.addEventListener("keydown", keyPush);
-    setInterval(game, 1000 / 15);
+    setInterval(game, 100);
 }
-px = py = 10;
+
+px = py = 10;   //X, Y, position player
 gs = tc = 20;
-ax = ay = 15;
-xv = yv = 0;
+ax = ay = 15;   //X, Y, postion food
+xv = yv = 0;    // X, Y, velocity
 trail = [];
-tail = 5;
+tail = 1;
+
 function game() {
     px += xv;
     py += yv;
@@ -19,20 +21,33 @@ function game() {
     if (px > tc - 1) {
         px = 0;
     }
-    if (py < 0) {
-        py = tc - 1;
+    if (py < 1) {
+        py = tc - 0;
     }
-    if (py > tc - 1) {
-        py = 0;
+    if (py > tc - 0) {
+        py = 1;
     }
+
+
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canv.width, canv.height);
 
-    ctx.fillStyle = "lime";
+    ctx.fillStyle = "grey";    
+    ctx.fillRect(0, 0, canv.width, 20);
+
+    ctx.fillStyle = "red";
+    ctx.font = "20px Arial";
+    ctx.fillText("SCORE: " + tail, 260, 17);
+
     for (var i = 0; i < trail.length; i++) {
+        if (i==trail.length-1){
+            ctx.fillStyle = "lime";        
+        }else{
+            ctx.fillStyle = "yellow";
+        }
         ctx.fillRect(trail[i].x * gs, trail[i].y * gs, gs - 2, gs - 2);
         if (trail[i].x == px && trail[i].y == py) {
-            tail = 5;
+            tail = 1;
         }
     }
     trail.push({ x: px, y: py });
@@ -43,11 +58,12 @@ function game() {
     if (ax == px && ay == py) {
         tail++;
         ax = Math.floor(Math.random() * tc);
-        ay = Math.floor(Math.random() * tc);
+        ay = Math.floor(Math.random() * ((tc-1) - 1)) + 1;
     }
     ctx.fillStyle = "red";
     ctx.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
 }
+
 function keyPush(evt) {
     switch (evt.keyCode) {
         case 37:
